50ce8889a151b605e2582e1b5ff2d53a0adb3845
anirudhmungre <anirudh.mungre16@gmail.com>
2020-04-04 15:25:05 -0400

    server with json

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,22 @@
 import * as express from "express";
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 5000;
-
 const app = express();
-app.set("port", PORT);
-
 const http = require("http").Server(app);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+app.set("port", PORT);
 
 app.get("/", (req: any, res: any) => {
     res.send("hello world");
+});
+
+app.post("/train", (req: any, res: any) => {
+    res.send(req.body);
 });
 
 http.listen(PORT, function() {
